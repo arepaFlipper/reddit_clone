536a8386850b02bf11891234da654f85f80d350e
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-11-26 13:39:58 -0500

    🐲 (1:36:20) Dev the DropdownMenu

--- src/components/UserAccountNav.tsx
+++ src/components/UserAccountNav.tsx
@@ -1,7 +1,8 @@
 import { User } from 'next-auth'
 import React from 'react'
-import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/DropdownMenu'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuItem } from '@/components/ui/DropdownMenu'
 import UserAvatar from '@/components/UserAvatar'
+import Link from 'next/link'
 
 type TUserAccountNav = {
   user: Pick<User, 'name' | 'image' | 'email'>
@@ -11,8 +12,33 @@ const UserAccountNav = ({ user }: TUserAccountNav) => {
   return (
     <DropdownMenu >
       <DropdownMenuTrigger>
-        <UserAvatar user={user} />
+        <UserAvatar user={{ name: user.name || null, image: user.image || null, }} className='w-8 h-8' />
       </DropdownMenuTrigger>
+      <DropdownMenuContent className='bg-white' align='end'>
+        <div className='flex items-center justify-start gap-2 p-2'>
+          <div className='flex flex-col space-y-1 leading-none'>
+            {(user.name) && (<p className='font-medium' >{user.name}</p>)}
+            {(user.email) && (<p className='w-[200px] truncate text-sm text-zinc-700' >{user.email}</p>)}
+          </div>
+        </div>
+
+        <DropdownMenuSeparator />
+
+        <DropdownMenuItem asChild>
+          <Link href={'/'} >Feed</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href={'/r/create'} >Create Community</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuItem asChild>
+          <Link href={'/settings'} >Settings</Link>
+        </DropdownMenuItem>
+
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className='cursor-pointer'>Sign out </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   )
 }
--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,12 +1,29 @@
 import { User } from 'next-auth'
+import { Avatar, AvatarFallback } from '@/components/ui/Avatar'
+import Image from 'next/image'
+import { Icons } from '@/components/Icons'
+import { AvatarProps } from '@radix-ui/react-avatar'
 
-type TUserAvatar = {
+
+interface TUserAvatar extends AvatarProps {
   user: Pick<User, 'name' | 'image'>
 }
 
-const UserAvatar = ({ user }: TUserAvatar) => {
+const UserAvatar = ({ user, ...props }: TUserAvatar) => {
   return (
-    <div>UserAvatar</div>
+    <Avatar {...props}>
+      {(user.image) ? (
+        <div className='relative aspect-square h-full w-full'>
+          <Image fill src={user.image} alt='profile picture' referrerPolicy='no-referrer' />
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span className='sr-only'>{user?.name}</span>
+          <Icons.user className='h-4 w-4' />
+        </AvatarFallback>
+      )
+      }
+    </Avatar >
   )
 }
 
