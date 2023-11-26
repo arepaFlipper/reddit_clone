64ee8696d9daecc1febc2c4e213f4dca02780e1a
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-11-26 16:35:15 -0500

    🛢️ (1:53:12)Implement the SignUp components

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,21 @@
+import { buttonVariants } from '@/components/ui/Button'
+import { ChevronLeft } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import SignIn from '@/components/SignIn'
+
+const page = () => {
+  return (
+    <div className="absolute inset-0">
+      <div className="h-full max-w-2xl mx-auto flex flex-col items-center justify-center gap-20">
+        <Link href="/" className={cn(buttonVariants({ variant: 'ghost' }), 'self-start -mt-20')}>
+          <ChevronLeft className="mr-2 h-4 w-4" />
+          Home
+        </Link>
+        <SignUp />
+      </div>
+    </div>
+  )
+}
+
+export default page;
