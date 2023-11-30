44fdb8f82814b99909f0393bfa7b129906635c2b
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-11-30 14:46:47 -0500

    🥌 (5:26:04) Display the Post compenent (only the component)

---
+++ src/components/Post.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+const Post = (props: Props) => {
+  return (
+    <div>Post</div>
+  )
+}
+
+export default Post
---
+++ src/components/PostFeed.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { useRef } from "react";
+import { ExtendedPost } from "@/types/db";
+import { useIntersection } from "@mantine/hooks";
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from "@/config";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { useSession } from "next-auth/react";
+import Post from "@/components/Post";
+
+type TPostFeed = {
+  initialPosts: ExtendedPost[];
+  subredditName?: string;
+};
+
+const PostFeed = ({ initialPosts, subredditName }: TPostFeed) => {
+  const { current } = useRef<HTMLElement>(null);
+  const { ref, entry } = useIntersection({ root: current, threshold: 1 });
+
+  const infinite_query = async ({ pageParam = 1 }) => {
+    const query = `/api/posts?limit=${INFINITE_SCROLLING_PAGINATION_RESULTS}&page=${pageParam}${(!!subredditName) && `&subredditName=${subredditName}`}`;
+    const { data } = await axios.get(query);
+    return data as ExtendedPost[];
+  }
+
+  const getNextPageParam = (_: unknown, pages: any) => {
+    return pages.length + 1;
+  }
+
+  const initialData = { pages: [initialPosts], pageParams: [1] };
+
+  const { data: session } = useSession();
+  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery(['infinite-query'], infinite_query, { getNextPageParam, initialData });
+  const posts = data?.pages.flatMap((page: any) => page) ?? initialPosts;
+  return (
+    <ul className="flex flex-col col-span-2 space-y-6">
+      {posts.map((post, index) => {
+        const votesAmt = post.votes.reduce((acc: number, vote: any) => {
+          if (vote.type === 'UP') return acc + 1;
+          if (vote.type === 'DOWN') return acc - 1;
+          return acc;
+
+        }, 0);
+        const currentVote = post.votes.find((vote: any) => vote.userId === session?.user.id);
+        if (index === posts.length - 1) {
+          return (
+            <li key={post.id} ref={ref}>
+              <Post />
+            </li>
+          )
+        } else {
+          return <Post />
+        }
+
+      })}
+    </ul>
+  )
+}
+
+
+export default PostFeed
