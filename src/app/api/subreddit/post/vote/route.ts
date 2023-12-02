a373be49491cde0652bddab1590a246aa58901dc
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-02 08:35:13 -0500

    💡 (6:34:00) vote/unvote endpoints

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,26 +1,56 @@
-import { PostVoteValidator } from "@/lib/validators/vote";
-import { getAuthSession } from "@/lib/auth";
-import { db } from "@/lib/db";
-import { CachedPost } from "@/types/redis";
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { redis } from '@/lib/redis'
+import { PostVoteValidator } from '@/lib/validators/vote'
+import { CachedPost } from '@/types/redis'
+import { z } from 'zod'
 
-const CACHE_AFTER_UPVOTES = 1;
+const CACHE_AFTER_UPVOTES = 1
+
+const recount_votes = async ({ post, voteType, postId }: any): Promise<{ votesAmt: number, cachePayload?: CachedPost }> => {
+
+  // Recount the votes
+  const votesAmt = post.votes.reduce((acc, vote) => {
+    if (vote.type === 'UP') return acc + 1
+    if (vote.type === 'DOWN') return acc - 1
+    return acc
+  }, 0)
+
+  if (votesAmt >= CACHE_AFTER_UPVOTES) {
+    const cachePayload: CachedPost = {
+      authorUsername: post.author.username ?? '',
+      content: JSON.stringify(post.content),
+      id: post.id,
+      title: post.title,
+      currentVote: null,
+      createdAt: post.createdAt,
+    }
+
+    await redis.hset(`post:${postId}`, cachePayload) // Store the post data as a hash
+  }
+
+  return { votesAmt };
+
+}
 
 export async function PATCH(req: Request) {
   try {
+    const body = await req.json()
 
-    const body = req.json();
-    const { postId, voteType } = PostVoteValidator.parse(body);
-    const session = await getAuthSession();
+    const { postId, voteType } = PostVoteValidator.parse(body)
+
+    const session = await getAuthSession()
 
     if (!session?.user) {
-      return new Response('Unauthorized', { status: 401 });
+      return new Response('🖕Unauthorized', { status: 401 })
     }
 
+    // check if user has already voted on this post
     const existingVote = await db.vote.findFirst({
       where: {
         userId: session.user.id,
         postId,
-      }
+      },
     })
 
     const post = await db.post.findUnique({
@@ -31,24 +61,30 @@ export async function PATCH(req: Request) {
         author: true,
         votes: true,
       },
-    });
+    })
 
     if (!post) {
-      return new Response('Post not found', { status: 404 });
+      return new Response('😰 Post not found', { status: 404 })
     }
 
     if (existingVote) {
+      // if vote type is the same as existing vote, delete the vote
       if (existingVote.type === voteType) {
         await db.vote.delete({
           where: {
             userId_postId: {
               postId,
               userId: session.user.id,
-            }
-          }
+            },
+          },
         })
-        return new Response('✅ Vote registered', { status: 200 });
+
+        recount_votes({ post, voteType, postId })
+
+        return new Response('💥 Post unvoted successfully ', { status: 200 })
       }
+
+      // if vote type is different, update the vote
       await db.vote.update({
         where: {
           userId_postId: {
@@ -58,34 +94,35 @@ export async function PATCH(req: Request) {
         },
         data: {
           type: voteType,
-        }
-      });
+        },
+      })
 
-      const votesAmt = post.votes.reduce((acc, vote) => {
-        if (vote.type === 'UP') {
-          return acc + 1;
-        }
+      recount_votes({ post, voteType, postId });
 
-        if (vote.type === "DOWN") {
-          return acc - 1;
-        }
-        return acc;
-      }, 0);
-
-      if (votesAmt >= CACHE_AFTER_UPVOTES) {
-        const cachePayload: CachedPost = {
-          authorUsername: post.author.username ?? "",
-          content: JSON.stringify(post.content),
-          id: post.id,
-          title: post.title,
-          currentVote: voteType,
-          createdAt: post.createdAt,
-        }
-      }
-
+      return new Response('🥳 Post voted successfully ', { status: 200 })
     }
-  } catch (error) {
-    console.error(error);
 
+    // if no existing vote, create a new vote
+    await db.vote.create({
+      data: {
+        type: voteType,
+        userId: session.user.id,
+        postId,
+      },
+    })
+
+    recount_votes({ post, voteType, postId });
+
+    return new Response('🙌 Post voted successfully ', { status: 200 })
+  } catch (error) {
+    (error)
+    if (error instanceof z.ZodError) {
+      return new Response(`😵‍💫 Invalid request data: ${error.message}`, { status: 400 })
+    }
+
+    return new Response(
+      '☠️  Could not post to subreddit at this time. Please try later',
+      { status: 500 }
+    );
   }
 }
--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -36,19 +36,19 @@ const PostVoteClient = ({ postId, initialVotesAmt, initialVote }: Props) => {
     await axios.patch('/api/subreddit/post/vote', payload);
   }
 
-  const { } = useMutation({
+  const { mutate } = useMutation({
     mutationFn,
   })
   return (
     <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
-      <Button size="sm" variant='ghost' aria-label='upvote'>
+      <Button onClick={() => mutate('UP')} size="sm" variant='ghost' aria-label='upvote'>
         <ArrowBigUp className={cn('h-5 w-5 text-zinc-700', { 'text-emerald-500 fill-emerald-500': currentVote === 'UP' })} />
       </Button>
 
       <p className="text-center py-2 font-medium text-sm text-zinc-900">
         {votesAmt}
       </p>
-      <Button size="sm" variant="ghost" aria-label="downvote">
+      <Button onClick={() => mutate('DOWN')} size="sm" variant="ghost" aria-label="downvote">
         <ArrowBigDown className={cn('h-5 w-5 text-zinc-700', { 'text-red-500 fill-red-500': currentVote === "DOWN" })} />
       </Button>
     </div>
