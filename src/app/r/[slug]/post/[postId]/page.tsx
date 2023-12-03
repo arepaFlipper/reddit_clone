05a9f11709c9d9191bfe2eea8d297471cea0d893
Cristian F. Tovar <cristian00tovar@gmail.com>
2023-12-03 18:12:00 -0500

    🍕 (7:33:40) detail the Post

--- src/app/r/[slug]/post/[postId]/page.tsx
+++ src/app/r/[slug]/post/[postId]/page.tsx
@@ -7,6 +7,7 @@ import { Suspense } from "react";
 import { buttonVariants } from '@/components/ui/Button';
 import { ArrowBigUp, Loader2, ArrowBigDown } from "lucide-react";
 import PostVoteServer from "@/components/post-vote/PostVoteServer";
+import { formatTimeToNow } from "@/lib/utils";
 
 interface IPage {
   params: {
@@ -51,8 +52,18 @@ const page = async ({ params }: IPage) => {
     <div>
       <div className="h-full flex flex-col sm:flex-row items-center sm:times-start justify-between">
         <Suspense fallback={<PostVoteShell />} >
+          {/* @ts-expect-error */}
           <PostVoteServer postId={post?.id ?? cachedPost.id} getData={getData} />
         </Suspense>
+        <div className="sm:w-0 w-full flex-1 bg-white p-4 rounded-sm">
+          <p className="max-h-40 mt-1 truncate text-xs text-gray-500">
+            Posted by u/{post?.author?.username ?? cachedPost.authorUsername}{' '}
+            {formatTimeToNow(new Date(post?.createdAt ?? cachedPost.createdAt))}
+          </p>
+          <h1 className="text-xl font-semibold py-2 leading-6 text-gray-900">
+            {post?.title ?? cachedPost.title}
+          </h1>
+        </div>
       </div>
     </div>
   )
